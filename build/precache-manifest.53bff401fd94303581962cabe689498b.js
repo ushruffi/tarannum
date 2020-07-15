46e902c90876baae8dd396ac010b54046adc9cfd
ushruffi <ushruffi@gmail.com>
2020-07-14 21:35:16 -0400

    Testing changes with CNAME

---
+++ build/precache-manifest.53bff401fd94303581962cabe689498b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6779f049dd07797217fe56b21b006d0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a261ced493ce133b4da",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1aaa6e94b2f6436f77d0",
+    "url": "/static/js/2.5b0c0ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b0c0ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a261ced493ce133b4da",
+    "url": "/static/js/main.6d6afeef.chunk.js"
+  },
+  {
+    "revision": "80261d8be39afafb2a75",
+    "url": "/static/js/runtime-main.2b199bf9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
