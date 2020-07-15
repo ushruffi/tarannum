7044534a36b21178fd3384fafbd9fc2f99c099c9
ushruffi <ushruffi@gmail.com>
2020-07-14 20:32:54 -0400

    Updates

---
+++ precache-manifest.d1e01821e56036b2b54519b1760216cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a12cbfbfa68610a7399fd9e180faec",
+    "url": "/tarannum/index.html"
+  },
+  {
+    "revision": "4762f77d2d61d2fdc9d1",
+    "url": "/tarannum/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1aaa6e94b2f6436f77d0",
+    "url": "/tarannum/static/js/2.5b0c0ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarannum/static/js/2.5b0c0ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4762f77d2d61d2fdc9d1",
+    "url": "/tarannum/static/js/main.ac85b619.chunk.js"
+  },
+  {
+    "revision": "f1f0563401f934deac5b",
+    "url": "/tarannum/static/js/runtime-main.9f20f739.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tarannum/static/media/logo.5d5d9eef.svg"
+  }
+]);
